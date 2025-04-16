bcb34eb8a3737894743b0c2db3206bc13f2b8226
dreamsofcoding <coding.dreams@pm.me>
2025-04-16 08:32:30 +0100

    setting up the project, updating libs/sdks and adding in a few bits and pieces

--- app/src/main/java/com/udacity/asteroidradar/Asteroid.kt
+++ app/src/main/java/com/udacity/asteroidradar/Asteroid.kt
@@ -2,9 +2,17 @@ package com.udacity.asteroidradar
 
 import android.os.Parcelable
 import kotlinx.android.parcel.Parcelize
+import kotlinx.serialization.Serializable
 
+@Serializable
 @Parcelize
-data class Asteroid(val id: Long, val codename: String, val closeApproachDate: String,
-                    val absoluteMagnitude: Double, val estimatedDiameter: Double,
-                    val relativeVelocity: Double, val distanceFromEarth: Double,
-                    val isPotentiallyHazardous: Boolean) : Parcelable
+data class Asteroid(
+    val id: Long,
+    val codename: String,
+    val closeApproachDate: String,
+    val absoluteMagnitude: Double,
+    val estimatedDiameter: Double,
+    val relativeVelocity: Double,
+    val distanceFromEarth: Double,
+    val isPotentiallyHazardous: Boolean
+) : Parcelable
